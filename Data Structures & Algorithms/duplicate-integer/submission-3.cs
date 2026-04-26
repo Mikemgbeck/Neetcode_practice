f0ad2783b8854dacacd75e25c405192cc6a9a7c1
Michael Beck <56134210+Mikemgbeck@users.noreply.github.com>
2026-04-26 09:46:07 -0600

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        HashSet<int> uniqueNumbers = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (!uniqueNumbers.Add(nums[i])) {
+                return true; // Duplicate found
+            }
+        }
+        return false; //no duplicates found
+    }
+}
