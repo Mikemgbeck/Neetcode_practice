57128780427f356918bfa80a2820661339087723
Michael Beck <56134210+Mikemgbeck@users.noreply.github.com>
2026-04-28 08:50:05 -0600

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] ReplaceElements(int[] arr) {
+        int rightMax = -1;
+        for (int i = arr.Length - 1; i >= 0; i--) {
+            int current = arr[i];
+            arr[i] = rightMax;
+            if (current > rightMax)
+            {
+                rightMax = current;
+            }
+        }
+        return arr;
+    }
+}
