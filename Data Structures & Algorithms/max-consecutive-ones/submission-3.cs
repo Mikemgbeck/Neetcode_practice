c2157fcf73742ddad172f768ed075e236cab5e23
Michael Beck <56134210+Mikemgbeck@users.noreply.github.com>
2026-04-28 07:52:53 -0600

    Add: max-consecutive-ones - submission-3

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int consecutive = 0;
+
+        //create a for loop over the length of the array
+        for (i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                consecutive++;
+
+                if (consecutive > max)
+                {
+                    max = consecutive;
+                }
+            }
+            else
+            {
+                consecutive = 0;
+            }
+        }
+        return consecutive;
+    }
+}
