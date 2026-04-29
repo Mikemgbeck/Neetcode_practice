15264484dffc3bbdd74310e6354e26901c2ef3ba
Michael Beck <56134210+Mikemgbeck@users.noreply.github.com>
2026-04-29 06:10:06 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>(s);
+        foreach( char bracket in s)
+        {
+            if(bracket == '{' || bracket == '(' || bracket == '[')
+            {
+                stack.Push(bracket);
+            }else if (stack.Count == 0 || bracket == '}')
+            {
+                if (stack.Pop() != '{')
+                {
+                    return false;
+                }
+            } else if ( stack.Count == 0 || bracket == ')')
+            {
+                if (stack.Pop() != '(')
+                {
+                    return false;
+                }
+            }else if (stack.Count == 0 || bracket == ']')
+            {
+                if (stack.Pop() != '[')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
