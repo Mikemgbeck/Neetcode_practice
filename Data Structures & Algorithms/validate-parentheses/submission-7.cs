bd6566967e4b10e23ade004a6be3157e12228ef9
Michael Beck <Mikemgbeck@gmail.com>
2026-04-29 06:30:40 -0700

    clean up

--- Data Structures & Algorithms/validate-parentheses/submission-7.cs
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public bool IsValid(string s) {
+        //this initilization with s caused me so many issues 
         var stack = new Stack<char>(s);
         foreach( char bracket in s)
         {
@@ -21,6 +22,29 @@ public class Solution {
             }
         }
         // this should be true for any successes 
+        return stack.Count == 0;
+    }
+}
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+
+        foreach (char c in s) {
+            if (c == ')') {
+                if (stack.Count == 0 || stack.Pop() != '(') return false;
+            } 
+            else if (c == ']') {
+                if (stack.Count == 0 || stack.Pop() != '[') return false;
+            } 
+            else if (c == '}') {
+                if (stack.Count == 0 || stack.Pop() != '{') return false;
+            } 
+            else {
+                // opening bracket
+                stack.Push(c);
+            }
+        }
+
         return stack.Count == 0;
     }
 }
