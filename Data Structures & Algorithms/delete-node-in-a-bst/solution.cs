e3db67fd2fc0ea55b6a78f5f78358001342e7f1b
Michael Beck <Mikemgbeck@gmail.com>
2026-05-04 09:06:41 -0600

    renames and comments

--- Data Structures & Algorithms/delete-node-in-a-bst/solution.cs
+++ Data Structures & Algorithms/delete-node-in-a-bst/solution.cs
@@ -15,25 +15,35 @@ public class Solution
 {
     public TreeNode DeleteNode(TreeNode root, int key) 
     {
+        // Base case: if the tree is empty
         if (root == null) return null;
 
+        // If the key to be deleted is greater than the root's key, then it lies in the right subtree
         if (key > root.val) 
         {
             root.right = DeleteNode(root.right, key);
+        // If the key to be deleted is smaller than the root's key, then it lies in the left subtree
         } else if (key < root.val) 
         {
             root.left = DeleteNode(root.left, key);
+        // If key is same as root's key, then this is the node to be deleted
         } else 
         {
+            // Node with only one child or no child
             if (root.left == null) return root.right;
+            // Node with only one child or no child
             if (root.right == null) return root.left;
 
+            // Node with two children: Get the inorder successor (smallest in the right subtree)
             TreeNode cur = root.right;
+            // Find the minimum value in the right subtree
             while (cur.left != null) 
             {
                 cur = cur.left;
             }
+            // Copy the inorder successor's(smallest in the right subtree) content to this node
             root.val = cur.val;
+            // Delete the inorder successor
             root.right = DeleteNode(root.right, root.val);
         }
         return root;
